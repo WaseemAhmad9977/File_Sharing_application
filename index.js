ca40a86f36c628c86fda479b1e922c3c765033b3
WaseemAhmad9977 <waseemahmad9977@gmail.com>
2025-03-23 12:47:41 +0530

    crud complete pre-practice

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
 app.use(express.static('view'))
-app.listen(3000,()=>{
-    console.log('3000 port is listening')
+app.listen(8080,()=>{
+    console.log('8080 port is listening')
 })
