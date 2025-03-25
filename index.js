00647d6b658f193d83e5a7a7a21367a2f622f433
WaseemAhmad9977 <waseemahmad9977@gmail.com>
2025-03-25 14:58:30 +0530

    upload-profile-and authentication secuirity jwt

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+import dotev from 'dotenv'
+dotev.config()
+
+import mongoose from "mongoose";
+mongoose.connect(process.env.DB_URL)
+
+
+
+import { login,signup,uploadProfilePicture } from './controller/user.controller.js';
+import { verifyToken } from './controller/token.controller.js';
+
 import express from 'express'
 const app = express()
-app.use(express.static('view'))
-app.listen(8080,()=>{
-    console.log('8080 port is listening')
+
+
+app.listen(8080)
+
+import bodyParser from 'body-parser'
+
+
+//middlewares
+app.use(express.static('view'));
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+app.use(bodyParser.json())
+
+//routes
+
+app.get('/api',(req,res)=>{
+    res.send('hello this is my home')
 })
+
+app.post('/api/login',login)
+
+app.post('/api/signup',signup)
+
+app.post('/api/verify-token',verifyToken)
+
+app.post('/api/upload-profile-picture',uploadProfilePicture)
---
+++ view/js/login.js
@@ -0,0 +1,24 @@
+const login = async (e) => {
+    e.preventDefault();
+    try {
+      const form = e.target;
+      const data = {
+          email:form.elements[0].value,
+          password:form.elements[1].value
+      }
+      // console.log(data)
+      const response = await axios.post("/api/login",data);
+      // console.log(response.data)
+      localStorage.setItem('auth',response.data.token)
+      
+      window.location = '/apps/dashboard.html'
+
+    } catch (err) {
+      Swal.fire({
+        icon: 'error',
+        title: "Error",
+        text: err.response?.data?.message || "Something went wrong!",
+    });
+    }
+};
+  
